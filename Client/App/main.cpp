0e3856793477da32716b688a11e679a3bcc9c119
luizantoniona <luizantonio.na@hotmail.com>
2026-05-30 16:34:37 -0300

    feat: starting client structure

--- Client/App/main.cpp
+++ Client/App/main.cpp
@@ -15,7 +15,7 @@ int main( int argc, char* argv[] ) {
 
     QQmlApplicationEngine engine;
     QObject::connect( &engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() { QCoreApplication::exit( -1 ); }, Qt::QueuedConnection );
-    engine.loadFromModule( "main", "Main" );
+    engine.loadFromModule( "MMORPGClient", "Main" );
 
     return app.exec();
 }
