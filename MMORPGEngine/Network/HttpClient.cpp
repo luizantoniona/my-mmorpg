4f304b8db350189d1fb997c592a88e09d0db1ee4
luizantoniona <luizantonio.na@hotmail.com>
2026-08-13 21:46:49 -0300

    fix: add httpclient in engine

---
+++ MMORPGEngine/Network/HttpClient.cpp
@@ -0,0 +1,66 @@
+#include "HttpClient.h"
+
+namespace Engine {
+
+HttpClient::HttpClient( QObject* parent ) :
+    QObject( parent ) {
+}
+
+void HttpClient::setBaseUrl( const QUrl& url ) {
+    _baseUrl = url;
+}
+
+QUrl HttpClient::baseUrl() const {
+    return _baseUrl;
+}
+
+QNetworkReply* HttpClient::get( const QString& endpoint, const QHash<QByteArray, QByteArray>& extraHeaders ) {
+    return _networkManager.get( buildRequest( buildUrl( endpoint ), extraHeaders ) );
+}
+
+QNetworkReply* HttpClient::getAuthenticated( const QString& endpoint, const QString& sessionId, const QHash<QByteArray, QByteArray>& extraHeaders ) {
+    auto headers = extraHeaders;
+    headers[ "Authorization" ] = QString( "X-Session %1" ).arg( sessionId ).toUtf8();
+    return get( endpoint, headers );
+}
+
+QNetworkReply* HttpClient::post( const QString& endpoint, const QByteArray& body, const QHash<QByteArray, QByteArray>& extraHeaders ) {
+    return _networkManager.post( buildRequest( buildUrl( endpoint ), extraHeaders ), body );
+}
+
+QNetworkReply* HttpClient::postAuthenticated( const QString& endpoint, const QByteArray& body, const QString& sessionId, const QHash<QByteArray, QByteArray>& extraHeaders ) {
+    auto headers = extraHeaders;
+    headers[ "Authorization" ] = QString( "X-Session %1" ).arg( sessionId ).toUtf8();
+    return post( endpoint, body, headers );
+}
+
+QUrl HttpClient::buildUrl( const QString& endpoint ) const {
+    QUrl url = _baseUrl;
+
+    QString path = url.path();
+
+    if ( !path.endsWith( '/' ) ) {
+        path += '/';
+    }
+
+    path += endpoint.startsWith( '/' ) ? endpoint.mid( 1 ) : endpoint;
+
+    url.setPath( path );
+
+    return url;
+}
+
+QNetworkRequest HttpClient::buildRequest( const QUrl& url, const QHash<QByteArray, QByteArray>& headers ) const {
+    QNetworkRequest request( url );
+
+    request.setHeader( QNetworkRequest::ContentTypeHeader, "application/json" );
+    request.setRawHeader( "Accept", "application/json" );
+
+    for ( auto it = headers.begin(); it != headers.end(); ++it ) {
+        request.setRawHeader( it.key(), it.value() );
+    }
+
+    return request;
+}
+
+} // namespace Engine
