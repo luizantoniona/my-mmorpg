2dc42b2905ad7a7308c664edaf97dc853904f705
luizantoniona <luizantonio.na@hotmail.com>
2026-08-13 21:46:25 -0300

    feat: create a network manager with a thread hosting the drogon server

--- MMORPGServer/Server/main.cpp
+++ MMORPGServer/Server/main.cpp
@@ -3,6 +3,7 @@
 #include <MMORPGEngine/Commons/Singleton.h>
 #include <MMORPGServer/Server/Database/Database.h>
 #include <MMORPGServer/Server/Manager/DataManager.h>
+#include <MMORPGServer/Server/Manager/NetworkManager.h>
 #include <MMORPGServer/Server/Manager/WorldManager.h>
 
 namespace {
@@ -22,15 +23,11 @@ int main() {
     // --- World ---
     Engine::Singleton<Server::WorldManager>::instance().initialize( DATA_PATH );
 
-    drogon::app()
-        .addListener( "0.0.0.0", 8080 )
-        .setThreadNum( std::thread::hardware_concurrency() )
-        .registerPostHandlingAdvice( []( const drogon::HttpRequestPtr& request, const drogon::HttpResponsePtr& response ) {
-            response->addHeader( "Access-Control-Allow-Origin", "*" );
-            response->addHeader( "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS" );
-            response->addHeader( "Access-Control-Allow-Headers", "Content-Type, Authorization" );
-        } )
-        .run();
+    // --- Network ---
+    Engine::Singleton<Server::NetworkManager>::instance().initialize();
+
+    while ( true ) {
+    }
 
     std::cout << "Ending Server" << std::endl;
 
