f945abc58b283e8eeeb5f15a173d561a65db4840
luizantoniona <luizantonio.na@hotmail.com>
2026-08-20 01:17:03 -0300

    feat: add new entity properties

--- MMORPGEngine/Entity/EntityModel.cpp
+++ MMORPGEngine/Entity/EntityModel.cpp
@@ -3,10 +3,20 @@
 namespace Engine {
 
 EntityModel::EntityModel() :
-    _position() {
+    _orientation(),
+    _position(),
+    _size() {
 }
 
 EntityModel::~EntityModel() = default;
+
+EntityOrientationModel EntityModel::orientation() const {
+    return _orientation;
+}
+
+void EntityModel::setOrientation( const EntityOrientationModel& orientation ) {
+    _orientation = orientation;
+}
 
 EntityPositionModel EntityModel::position() const {
     return _position;
@@ -14,6 +24,14 @@ EntityPositionModel EntityModel::position() const {
 
 void EntityModel::setPosition( const EntityPositionModel& position ) {
     _position = position;
+}
+
+EntitySizeModel EntityModel::size() const {
+    return _size;
+}
+
+void EntityModel::setSize( const EntitySizeModel& size ) {
+    _size = size;
 }
 
 } // namespace Engine
