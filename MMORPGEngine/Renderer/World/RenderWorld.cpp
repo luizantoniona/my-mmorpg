72ef5cff5d2287c3ac88b1547807d4ff257fcfb3
luizantoniona <luizantonio.na@hotmail.com>
2026-09-11 00:52:25 -0300

    feat: create has functions for renderworld

--- MMORPGEngine/Renderer/World/RenderWorld.cpp
+++ MMORPGEngine/Renderer/World/RenderWorld.cpp
@@ -6,4 +6,12 @@ RenderWorld::RenderWorld( QObject* parent ) :
     QObject( parent ) {
 }
 
+bool RenderWorld::hasObject( int x, int y, int z ) const {
+    return object( x, y, z ) != nullptr;
+}
+
+bool RenderWorld::hasTile( int x, int y, int z ) const {
+    return tile( x, y, z ) != nullptr;
+}
+
 } // namespace Engine
--- MMORPGEngine/Renderer/World/RenderWorld.h
+++ MMORPGEngine/Renderer/World/RenderWorld.h
@@ -17,6 +17,9 @@ public:
     virtual const WorldObjectModel* object( int x, int y, int z ) const = 0;
 
     virtual const WorldTileModel* tile( int x, int y, int z ) const = 0;
+
+    Q_INVOKABLE bool hasObject( int x, int y, int z ) const;
+    Q_INVOKABLE bool hasTile( int x, int y, int z ) const;
 };
 
 } // namespace Engine
