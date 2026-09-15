40cc14b82eab014d7b3d12b07e4afea01b91f61a
luizantoniona <luizantonio.na@hotmail.com>
2026-09-14 22:31:54 -0300

    feat: can work with character in the client app

--- MMORPGServer/Server/Network/Rest/DataController.h
+++ MMORPGServer/Server/Network/Rest/DataController.h
@@ -9,7 +9,7 @@ class DataController : public drogon::HttpController<DataController> {
 public:
     METHOD_LIST_BEGIN
     ADD_METHOD_TO( DataController::downloadManifest, "/data/manifest", drogon::Get, drogon::Options, "Server::AuthFilter" );
-    ADD_METHOD_TO( DataController::downloadData, "/data/{1:path}", drogon::Get, drogon::Options, "Server::AuthFilter" );
+    ADD_METHOD_VIA_REGEX( DataController::downloadData, "/data/(.*)", drogon::Get, drogon::Options, "Server::AuthFilter" );
     METHOD_LIST_END
 
     void downloadManifest( const drogon::HttpRequestPtr& request, std::function<void( const drogon::HttpResponsePtr& )>&& callback ) const;
