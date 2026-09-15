f7e6c6ea86184ee5327e46636f0f30b8b34b541c
luizantoniona <luizantonio.na@hotmail.com>
2026-09-14 22:24:08 -0300

    feat: save server data on client side by the server host and port - client could connect to other server with different maps and different data

--- MMORPGClient/Client/Application/Sync/SyncPageControl.h
+++ MMORPGClient/Client/Application/Sync/SyncPageControl.h
@@ -1,14 +1,20 @@
 #ifndef SYNCPAGECONTROL_H
 #define SYNCPAGECONTROL_H
 
+#include <QList>
 #include <QObject>
+
+#include <MMORPGEngine/Data/Manifest/ManifestDataDTO.h>
 
 class SyncPageControl : public QObject {
     Q_OBJECT
+    Q_PROPERTY( qreal progress READ progress NOTIFY progressChanged )
 
 public:
     explicit SyncPageControl( QObject* parent = nullptr );
     ~SyncPageControl();
+
+    qreal progress() const;
 
 public slots:
     void sync();
@@ -17,6 +23,19 @@ signals:
     void syncUpdate( const QString& message );
     void syncSucceeded();
     void syncFailed( const QString& error );
+
+    void progressChanged();
+
+private:
+    void downloadNext();
+
+    void setProgress( qreal progress );
+
+private:
+    QList<Engine::ManifestDataDTO> _downloadQueue;
+    int _downloadedCount;
+    int _totalCount;
+    qreal _progress;
 };
 
 #endif // SYNCPAGECONTROL_H
